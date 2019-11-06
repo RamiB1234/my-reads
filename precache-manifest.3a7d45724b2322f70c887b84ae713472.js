fea84846e926c49c6a579d3918577fc5ff49b2c0
Rami Bokhari <rbisthebest@hotmail.com>
2019-11-06 12:14:53 +0300

    Updates

---
+++ precache-manifest.3a7d45724b2322f70c887b84ae713472.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/my-reads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/my-reads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/my-reads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "d3c91a85274073bd8c52",
+    "url": "/my-reads/static/js/runtime~main.d3c91a85.js"
+  },
+  {
+    "revision": "ac412295e5efe7710e76",
+    "url": "/my-reads/static/js/main.ac412295.chunk.js"
+  },
+  {
+    "revision": "45b0cd3d381e311d81fb",
+    "url": "/my-reads/static/js/1.45b0cd3d.chunk.js"
+  },
+  {
+    "revision": "ac412295e5efe7710e76",
+    "url": "/my-reads/static/css/main.eae7a4e2.chunk.css"
+  },
+  {
+    "revision": "8608c73d7024834d237211b68524521f",
+    "url": "/my-reads/index.html"
+  }
+];
